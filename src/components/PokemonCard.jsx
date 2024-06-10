a7b259cf0449a933ce8626dac748282513f83d2e
Medhi <medhi.ikherbane@gmail.com>
2024-06-10 23:43:39 +0200

    add new component.1 with pokecard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,7 @@
 
 
 function PokemonCard() {
-    return (<div> className= "Pokemon"
+    return (<div>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
             <figcaption>Bulbasaur</figcaption>
     </div>
