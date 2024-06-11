75a85d0c40fbf9180e81eaea0fc34fef86ab825c
Medhi <medhi.ikherbane@gmail.com>
2024-06-11 10:54:25 +0200

    add new const

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,23 @@
 
 
-function PokemonCard() {
-    return (<div>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>Bulbasaur</figcaption>
-    </div>
-    );
-  };
-  
-  export default PokemonCard;
+  const pokemonList = [
+        {
+          name: "bulbasaur",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
+      
+        const PokemonCard = () => {
+        const pokemon = pokemonList[0];
+        console.log(pokemon.name);
+        return ( <div>
+                {pokemon.imgSrc ? <><img src={pokemon.imgSrc} alt={pokemon.name} /><p>{pokemon.name}</p></> 
+                : <p>???</p>}
+                </div>
+        )
+        }
+        export default PokemonCard
