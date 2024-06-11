8f1933f66ce4727b22c3b1c824aefb1a30590f90
Medhi <medhi.ikherbane@gmail.com>
2024-06-11 14:31:21 +0200

    add props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,16 @@
 
 
-  const pokemonList = [
-        {
-          name: "bulbasaur",
-          imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-          name: "mew",
-        },
-      ];
+  
       
-        const PokemonCard = () => {
-        const pokemon = pokemonList[0];
-        console.log(pokemon.name);
+        const PokemonCard = ({pokemon}) => {
+
         return ( <div>
                 {pokemon.imgSrc ? <><img src={pokemon.imgSrc} alt={pokemon.name} /><p>{pokemon.name}</p></> 
                 : <p>???</p>}
                 </div>
         )
         }
+
+      
+
         export default PokemonCard
