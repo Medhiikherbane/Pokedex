f7fbe72076795b84390900c78517cf2686f9def8
Medhi <medhi.ikherbane@gmail.com>
2024-06-12 15:46:09 +0200

    add prop-types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,22 @@
+import PropTypes from "prop-types"
+import "../App.css"
 
+const PokemonCard = ({pokemon}) => {
 
-  
-      
-        const PokemonCard = ({pokemon}) => {
+return ( <div className="card" >
+   {pokemon.imgSrc ? <><img className="card-img" src={pokemon.imgSrc} alt={pokemon.name} /><p>{pokemon.name}</p></> 
+    : <p>???</p>}
+    </div>
+	)
+}
 
-        return ( <div>
-                {pokemon.imgSrc ? <><img src={pokemon.imgSrc} alt={pokemon.name} /><p>{pokemon.name}</p></> 
-                : <p>???</p>}
-                </div>
-        )
-        }
+PokemonCard.PropTypes = {
+	pokemon: PropTypes.arrayOf (
+		PropTypes.shape ({
+			name: PropTypes.string.isRequired,
+			imgSrc: PropTypes.string
+		})
+	).isRequired
+}      
 
-      
-
-        export default PokemonCard
+export default PokemonCard
